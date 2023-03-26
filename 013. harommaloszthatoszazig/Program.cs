77253d5a7842979bd7195640e4734aa3b247bdd5
Dömötör Gyula <grattlesnake@gmail.com>
2023-03-26 14:32:54 +0200

    100-ig a 3-mal osztható számok

---
+++ 013. harommaloszthatoszazig/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013.harommaloszthatoszazig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+            Console.ReadKey();
+        }
+    }
+}
